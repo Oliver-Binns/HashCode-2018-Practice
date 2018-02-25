262575067662bb101de5ed53b7713dc1027cf1ec
Oliver Binns <mail@oliverbinns.co.uk>
2018-02-25 19:18:21 +0000

    Added output-to-file function to pizza object.

--- hashCodePractice.py
+++ hashCodePractice.py
@@ -5,20 +5,60 @@ class Ingredient(Enum):
     Tomato = 1
     Mushroom = 2
 
+class Pizza:
+    def __init__(self, f):
+        rows, cols, self.min_ings, self.max_cells = map(
+            lambda x: int(x), f.readline().split(' ')
+        )
+        
+        self.layout = [
+            [Ingredient.Empty for _ in range(cols)] 
+            for _ in range(rows)
+        ]
+        
+        for i, line in enumerate(f):
+            for j, ingredient in enumerate(line):
+                if "T" in ingredient:
+                    self.layout[i][j] = Ingredient.Tomato
+                elif "M" in ingredient:
+                    self.layout[i][j] = Ingredient.Mushroom
+            
+        self.slices = []
+                        
+    def __repr__(self):
+        return str(self.layout)
+        
+    def slice(self, topLeft, bottomRight):
+        self.slices.append(Pizza.Slice(topLeft, bottomRight))
+        
+    def output(self, f):
+        f.write("{:d}\n".format(len(self.slices)))
+        for slice in self.slices:
+            f.write(str(slice) + "\n")
+            
+    
+    class Slice:
+        def __init__(self, topLeft, bottomRight):
+            self.topLeft = topLeft
+            self.bottomRight = bottomRight
+            
+        def __repr__(self):
+            return "{:d} {:d} {:d} {:d}".format(
+                self.topLeft[0], self.topLeft[1], 
+                self.bottomRight[0], self.bottomRight[1]
+            )
+            
+
 def main():
-    f = open('input/example.in', 'r')
-    text = f.readline()
-    rows, cols, min, max = map(lambda x: int(x), text.split(' '))
+    i_f = open('input/example.in', 'r')
+    pizza = Pizza(i_f)
+    i_f.close()
     
-    pizza = [[Ingredient.Empty for _ in range(cols)] for _ in range(rows)]
+    # Insert logic to slice up pizza here:
+    #pizza.slice((0,0), (2,2))
     
-    for i, line in enumerate(f):
-        for j, ingredient in enumerate(line):
-            if "T" in ingredient:
-                pizza[i][j] = Ingredient.Tomato
-            elif "M" in ingredient:
-                pizza[i][j] = Ingredient.Mushroom
-    
-    f.close()
+    outname = i_f.name.replace("input/", "").replace(".in", "")
+    o_f = open('output/'+outname+'.out', 'w')
+    pizza.output(o_f)
     
 main()
