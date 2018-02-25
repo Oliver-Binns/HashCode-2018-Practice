50883b4247e5a495bec83362d4c2fdc52c713764
Oliver Binns <mail@oliverbinns.co.uk>
2018-02-25 18:33:38 +0000

    Refactored to reduce LOC

--- hashCodePractice.py
+++ hashCodePractice.py
@@ -1,51 +1,24 @@
-def readToStartOfNextLine(text, i):
-    while i < len(text) and not text[i] == '\n':
-        i += 1
-    i += 1
-    return i
+from enum import Enum
+
+class Ingredient(Enum):
+    Empty = 0
+    Tomato = 1
+    Mushroom = 2
 
 def main():
     f = open('input/example.in', 'r')
-
-    text = f.read()
-    i = 0
-
-    print(text)
-
-    r = text[i]
-    while (text[i] != ' '):
-        i += 1
-    i += 1
-    c = text[i]
-    while (text[i] != ' '):
-        i += 1
-    i += 1
-    l = text[i]
-    while (text[i] != ' '):
-        i += 1
-    i += 1
-    h = text[i]
-
-    print("rows =", r)
-    print("columns =", c)
-    print("low =", l)
-    print("high =", h)
-
-    rowRange = range(int(r))
-    colRange = range(int(c))
-    pizza = [['\0' for _ in colRange] for _ in rowRange]
-
-    i = readToStartOfNextLine(text, i)
-
-    for row in rowRange:
-        for col in colRange:
-            pizza[row][col] = text[i]
-            i += 1
-
-        i = readToStartOfNextLine(text, i);
-
-    print(pizza)
-
+    text = f.readline()
+    rows, cols, min, max = map(lambda x: int(x), text.split(' '))
+    
+    pizza = [[Ingredient.Empty for _ in range(cols)] for _ in range(rows)]
+    
+    for i, line in enumerate(f):
+        for j, ingredient in enumerate(line):
+            if "T" in ingredient:
+                pizza[i][j] = Ingredient.Tomato
+            elif "M" in ingredient:
+                pizza[i][j] = Ingredient.Mushroom
+    
     f.close()
-
+    
 main()
